253f6038bbaae823848d9eb36bad01667aeda8f4
Arbalest00 <qaz1252953393@gmail.com>
2023-10-21 21:46:46 +0800

    update

--- code_python/FlightControll/communication/serial_protocol.py
+++ code_python/FlightControll/communication/serial_protocol.py
@@ -9,16 +9,23 @@ from func.global_variable import lock,task_start_sign
 DEBUG=False
 class Serial_fc(object):
     def __init__(self,port,baudrate):
-        self.ser=serial.Serial(port=port,baudrate=baudrate)
+        self.ser=serial.Serial(port=port,baudrate=baudrate,timeout=2)
         self.fclisten_running=False
         self.fcsend_running=False
+        self.connected=False
         self.startbyte=b'\xAA'
         self.endbyte=0xFF
     def port_open(self):
+        #只要飞控处于上电状态，发送函数就会一直发送数据，所以在启动串口时接收一次串口数据，如果收到数据则说明串口连接正常
         self.ser.close()
         if self.ser.is_open==False:
             self.ser.open()
-            logger.info("目前飞控串口状态：%s",self.ser.is_open)
+        while self.connected==False:
+            try:
+                self.ser.read()
+                self.connected=True
+            except serial.SerialTimeoutException:
+                logger.error("飞控串口接收超时")
     def listen_start(self,rxbuffer:List[int]):
         self.fclisten_running=True
         listen_thread=threading.Thread(target=Serial_fc.listen_fc,args=(self,rxbuffer))
@@ -32,7 +39,7 @@ class Serial_fc(object):
         while self.fclisten_running ==True:
             byte_data = self.ser.read() 
             if byte_data == self.startbyte:
-                # 读取接下来的四个字节数据
+                # 读取接下来的数据
                 recv = self.ser.read(6)
                 # 判断数据是否符合通信协议，即以0xFF结尾
                 if recv[5] == self.endbyte:
--- code_python/FlightControll/func/global_variable.py
+++ code_python/FlightControll/func/global_variable.py
@@ -2,4 +2,4 @@ import threading
 from multiprocessing import Value
 speed_bias=51 #串口传输速度偏置量
 lock=threading.Lock()#线程锁
-task_start_sign=Value("b",False)
+task_start_sign=Value("b",False)#飞控阶段标志位 当进入程序控制阶段时变为True
