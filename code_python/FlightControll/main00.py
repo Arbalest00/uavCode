f5bef593023e8d2c24d069350ff3985e8add7098
Arbalest00 <qaz1252953393@gmail.com>
2023-10-22 16:15:34 +0800

    up

--- code_python/FlightControll/main00.py
+++ code_python/FlightControll/main00.py
@@ -10,8 +10,8 @@ from communication.serial_protocol import Serial_fc,Serial_gpio
 import time
 from func.Logger import logger
 from external_device.RadarDrivers_reconstruct.Radar import Radar
-#from Lcode.Lmission.Lmission_final import mission
-##############################################变量##################    ##########################
+from mission.Mission import mission
+##############################################变量############################################
 rxbuffer_fc=[0,0,0]#飞控反传信息 任务模式/x积分值/y积分值
 rxbuffer_gpio=[0,0,0,0]#gpio反传信息,启动/P1/P2/模式
 com_fc = [170, 0, speed_bias, speed_bias, 120, speed_bias, 0,speed_bias, 255]#发送给飞控的数据 帧头/启动命令/x/y/z高度/yaw/任务切换标志位/速度偏置量/帧尾
@@ -24,14 +24,13 @@ serial_fc=Serial_fc("/dev/ttyAMA0",460800)
 serial_fc.port_open()
 serial_fc.listen_start(rxbuffer_fc)
 serial_fc.send_start(com_fc)
-serial_gpio=Serial_gpio("/dev/ttyUSBArduino",38400)
-#serial_gpio.port_open()
-#serial_gpio.send_start(com_gpio)
-#serial_gpio.listen_start(rxbuffer_gpio)
-#terminal=communication.udp_protocol.udp_terminal()
-#terminal.listen_start(recv_address,recv_port)
-""" radar=Radar()
-radar.start('COM3','LD06')
-radar.start_resolve_pose() """
+""" serial_gpio=Serial_gpio("/dev/ttyUSBArduino",38400)
+serial_gpio.port_open()
+serial_gpio.send_start(com_gpio)
+serial_gpio.listen_start(rxbuffer_gpio) """
+""" terminal=communication.udp_protocol.udp_terminal()
+terminal.listen_start(recv_address,recv_port) """
+mission=mission(rxbuffer_fc,com_fc,com_gpio,rxbuffer_gpio)
+mission.run()
 while(1):
      time.sleep(0.1)
